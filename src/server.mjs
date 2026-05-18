e42c65e24f57013d45c93042950509828aef5131
Caraxes <mateenavarro23@gmail.com>
2026-05-18 18:47:47 -0300

    Correcciones TP2

--- src/server.mjs
+++ src/server.mjs
@@ -1,11 +1,15 @@
 import express from 'express'
+import path from 'path' // manipular y unir rutas de carpetas de forma segura
+import { fileURLToPath } from 'url' // convierte una URL (file://...) a una ruta normal del sistema operativo (C:/...)
 
 const app = express()
-
 const PUERTO = 3000
 
-app.use(express.static('public')) //muestra el front 
+const __filename = fileURLToPath(import.meta.url) // ruta absoluta exacta de server.mjs
+const __dirname = path.dirname(__filename) // ruta absoluta de la carpeta src
+
+app.use(express.static(path.join(__dirname, 'public'))) // Le suma a la carpeta src 'public' --> C:/.../src/public
 
 app.listen(PUERTO, () => {
-    console.log(`servidor corriendo en http://localhost:${PUERTO}`)
+    console.log(`Servidor corriendo en http://localhost:${PUERTO}`)
 })
