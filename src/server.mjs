00ca19e3af0b4c535e52a5f6f5726f5f7aa34fff
Caraxes <mateenavarro23@gmail.com>
2026-06-04 20:29:09 -0300

    Reestructuracion Front

--- src/server.mjs
+++ src/server.mjs
@@ -16,8 +16,14 @@ const __filename = fileURLToPath(import.meta.url) // ruta absoluta exacta de ser
 const __dirname = path.dirname(__filename) // ruta absoluta de la carpeta src
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public'))) // Le suma a la carpeta src 'public' --> C:/.../src/public
 
+// Rutas de Front
+// Le suma a la carpeta --> C:/.../src/
+app.use('/recursos', express.static(path.join(__dirname, './public/recursos')))
+app.use('/admin', express.static(path.join(__dirname, './public/admin')))
+app.use('/', express.static(path.join(__dirname, './public/tienda')))
+
+// Rutas de tu API
 app.use(rutasProductos)
 app.use(rutasCategorias)
 app.use(rutasTalles)
@@ -25,5 +31,5 @@ app.use(rutasColores)
 
 app.listen(PUERTO, () => {
     console.log(`Servidor corriendo en http://localhost:${PUERTO}`)
-    console.log(`Panel de Admin http://localhost:${PUERTO}/admin.html`)
+    console.log(`Panel de Admin http://localhost:${PUERTO}/admin`)
 })
