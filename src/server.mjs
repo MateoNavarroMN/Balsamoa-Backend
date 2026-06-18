6ac1bdb7cd28bf5f38ff199de60bd357d47afb64
Caraxes <mateenavarro23@gmail.com>
2026-06-18 18:08:12 -0300

    Endpoints de Autenticacion

--- src/server.mjs
+++ src/server.mjs
@@ -2,12 +2,16 @@ import express from 'express'
 import path from 'path' // manipular y unir rutas de carpetas de forma segura
 import { fileURLToPath } from 'url' // convierte una URL (file://...) a una ruta normal del sistema operativo (C:/...)
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
 // Imports de Rutas de cada modulo
 import rutasProductos from './modulos/productos/rutas.productos.mjs'
 import rutasCategorias from './modulos/categorias/rutas.categorias.mjs'
 import rutasTalles from './modulos/talles/rutas.talles.mjs'
 import rutasColores from './modulos/colores/rutas.colores.mjs'
+import rutasAutenticacion from './modulos/autenticacion/rutas.autenticacion.mjs'
+
+dotenv.config()
 
 const app = express()
 const PUERTO = process.env.PUERTO || 3000
@@ -16,6 +20,8 @@ const __filename = fileURLToPath(import.meta.url) // ruta absoluta exacta de ser
 const __dirname = path.dirname(__filename) // ruta absoluta de la carpeta src
 
 app.use(express.json())
+app.use(cookieParser())
+
 
 // Rutas de Front
 // Le suma a la carpeta --> C:/.../src/
@@ -24,6 +30,7 @@ app.use('/admin', express.static(path.join(__dirname, './public/admin')))
 app.use('/', express.static(path.join(__dirname, './public/tienda')))
 
 // Rutas de tu API
+app.use(rutasAutenticacion)
 app.use(rutasProductos)
 app.use(rutasCategorias)
 app.use(rutasTalles)
